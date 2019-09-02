4e4fa579270d3683f9b583954d448a661d412cce
Ren12 <stereotrip12@gmail.com>
2019-09-02 03:03:01 +0300

    khjkhjk
    hjkhjk

--- js/main.js
+++ js/main.js
@@ -22716,4 +22716,45 @@ $(document).ready(function () {
             });
         }, 300);
     }
+
+    myMap.behaviors.disable('scrollZoom');
+
+    var ctrlKey = false;
+    var ctrlMessVisible = false;
+    var timer;
+
+// Отслеживаем скролл мыши на карте, чтобы показывать уведомление
+    myMap.events.add(['wheel', 'mousedown'], function(e) {
+        if (e.get('type') == 'wheel') {
+            if (!ctrlKey) { // Ctrl не нажат, показываем уведомление
+                $('#ymap_ctrl_display').fadeIn(300);
+                ctrlMessVisible = true;
+                clearTimeout(timer); // Очищаем таймер, чтобы продолжать показывать уведомление
+                timer = setTimeout(function() {
+                    $('#ymap_ctrl_display').fadeOut(300);
+                    ctrlMessVisible = false;
+                }, 1500);
+            }
+            else { // Ctrl нажат, скрываем сообщение
+                $('#ymap_ctrl_display').fadeOut(100);
+            }
+        }
+        if (e.get('type') == 'mousedown' && ctrlMessVisible) { // Скрываем уведомление при клике на карте
+            $('#ymap_ctrl_display').fadeOut(100);
+        }
+    });
+
+// Обрабатываем нажатие на Ctrl
+    $(document).keydown(function(e) {
+        if (e.which === 17 && !ctrlKey) { // Ctrl нажат: включаем масштабирование мышью
+            ctrlKey = true;
+            myMap.behaviors.enable('scrollZoom');
+        }
+    });
+    $(document).keyup(function(e) { // Ctrl не нажат: выключаем масштабирование мышью
+        if (e.which === 17) {
+            ctrlKey = false;
+            myMap.behaviors.disable('scrollZoom');
+        }
+    });
 });
